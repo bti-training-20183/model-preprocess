1402aa8cf065aed9cb99ab30e2ca75a4fb428d9d
hoanghihi <a2k40pbc@gmail.com>
2019-07-18 13:22:17 +0700

    added features_adding

---
+++ preprocess_input.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from preprocessing_utils import *
+
+class DataCleaning():
+    def __init__(self, csv_filename):
+        self.csv_filename = csv_filename
+        self.data_frame = pd.read_csv(csv_filename)
+    
+    def handle_missing_data(self):
+        self.data_frame.fillna(0)
+    
+    def handle_outlier_data(self):
+        # TODO
+        pass
+
---
+++ preprocessing_utils.py
@@ -0,0 +1,159 @@
+import numpy as np
+import pandas as pd
+
+def get_file_extension(filename):
+    file_extension = "." + filename.rpartition(".")[-1]
+    return file_extension
+
+def convert_file_to_csv(filename):
+    file_extension = get_file_extension(filename)
+    if file_extension == '.txt':
+        txt_data_frame = pd.read_csv(filename, delimiter=",")
+        txt_data_frame.reset_index
+        txt_data_frame.to_csv(filename.replace(file_extension, '.csv'))
+    elif file_extension == '.xls' or file_extension == '.xlsx':
+        exel_data_frame = pd.read_excel(filename, index_col=None)
+        exel_data_frame.to_csv(filename.replace(file_extension, '.csv'), encoding='utf-8', index=False)
+
+
+def calculate_rsi(values):
+    up = values[values>0].mean()
+    down = -1*values[values<0].mean()
+    return 100*up/(up+down)
+
+def bbands(price, length=30, numsd=2):
+    ave = price.rolling(window = length, center = False).mean()
+    sd = price.rolling(window = length, center = False).std()
+    upband = ave + (sd*numsd)
+    dnband = ave - (sd*numsd)
+    return np.round(ave,3), np.round(upband,3), np.round(dnband,3)
+
+def psar(df, iaf = 0.02, maxaf = 0.2):
+    length = len(df)
+    dates = (df['Date'])
+    high = (df['High'])
+    low = (df['Low'])
+    close = (df['Close'])
+    psar = df['Close'][0:len(df['Close'])]
+    psarbull = [None] * length
+    psarbear = [None] * length
+    bull = True
+    af = iaf
+    ep = df['Low'][0]
+    hp = df['High'][0]
+    lp = df['Low'][0]
+    for i in range(2,length):
+        if bull:
+            psar[i] = psar[i - 1] + af * (hp - psar[i - 1])
+        else:
+            psar[i] = psar[i - 1] + af * (lp - psar[i - 1])
+        reverse = False
+        if bull:
+            if df['Low'][i] < psar[i]:
+                bull = False
+                reverse = True
+                psar[i] = hp
+                lp = df['Low'][i]
+                af = iaf
+        else:
+            if df['High'][i] > psar[i]:
+                bull = True
+                reverse = True
+                psar[i] = lp
+                hp = df['High'][i]
+                af = iaf
+        if not reverse:
+            if bull:
+                if df['High'][i] > hp:
+                    hp = df['High'][i]
+                    af = min(af + iaf, maxaf)
+                if df['Low'][i - 1] < psar[i]:
+                    psar[i] = df['Low'][i - 1]
+                if df['Low'][i - 2] < psar[i]:
+                    psar[i] = df['Low'][i - 2]
+            else:
+                if df['Low'][i] < lp:
+                    lp = df['Low'][i]
+                    af = min(af + iaf, maxaf)
+                if df['High'][i - 1] > psar[i]:
+                    psar[i] = df['High'][i - 1]
+                if df['High'][i - 2] > psar[i]:
+                    psar[i] = df['High'][i - 2]
+        if bull:
+            psarbull[i] = psar[i]
+        else:
+            psarbear[i] = psar[i]
+    df['psar'] = psar
+
+def aroon(df, tf=25):
+    aroonup = []
+    aroondown = []
+    x = tf
+    while x< len(df['Date']):
+        aroon_up = ((df['High'][x-tf:x].tolist().index(max(df['High'][x-tf:x])))/float(tf))*100
+        aroon_down = ((df['Low'][x-tf:x].tolist().index(min(df['Low'][x-tf:x])))/float(tf))*100
+        aroonup.append(aroon_up)
+        aroondown.append(aroon_down)
+        x+=1
+    return aroonup, aroondown
+
+
+def CMFlow(df, tf):
+    CHMF = []
+    MFMs = []
+    MFVs = []
+    x = tf
+
+    while x < len(df['Date']):
+        PeriodVolume = 0
+        volRange = df['Volume'][x - tf:x]
+        for eachVol in volRange:
+            PeriodVolume += eachVol
+
+        MFM = ((df['Close'][x] - df['Low'][x]) - (df['High'][x] - df['Close'][x])) / (df['High'][x] - df['Low'][x])
+        MFV = MFM * PeriodVolume
+
+        MFMs.append(MFM)
+        MFVs.append(MFV)
+        x += 1
+
+    y = tf
+    while y < len(MFVs):
+        PeriodVolume = 0
+        volRange = df['Volume'][x - tf:x]
+        for eachVol in volRange:
+            PeriodVolume += eachVol
+        consider = MFVs[y - tf:y]
+        tfsMFV = 0
+
+        for eachMFV in consider:
+            tfsMFV += eachMFV
+
+        tfsCMF = tfsMFV / PeriodVolume
+        CHMF.append(tfsCMF)
+        y += 1
+    return CHMF
+
+def CCI(df, n, constant):
+    TP = (df['High'] + df['Low'] + df['Close']) / 3
+    CCI = pd.Series((TP - TP.rolling(window=n, center=False).mean()) / (constant * TP.rolling(window=n, center=False).std())) #, name = 'CCI_' + str(n))
+    return CCI
+
+def KELCH(df, n):
+    KelChM = pd.Series(((df['High'] + df['Low'] + df['Close']) / 3).rolling(window =n, center=False).mean(), name = 'KelChM_' + str(n))
+    KelChU = pd.Series(((4 * df['High'] - 2 * df['Low'] + df['Close']) / 3).rolling(window =n, center=False).mean(), name = 'KelChU_' + str(n))
+    KelChD = pd.Series(((-2 * df['High'] + 4 * df['Low'] + df['Close']) / 3).rolling(window =n, center=False).mean(), name = 'KelChD_' + str(n))
+    return KelChM, KelChD, KelChU
+
+def DMI(df, period):
+    df['UpMove'] = df['High'] - df['High'].shift(1)
+    df['DownMove'] = df['Low'].shift(1) - df['Low']
+    df['Zero'] = 0
+
+    df['PlusDM'] = np.where((df['UpMove'] > df['DownMove']) & (df['UpMove'] > df['Zero']), df['UpMove'], 0)
+    df['MinusDM'] = np.where((df['UpMove'] < df['DownMove']) & (df['DownMove'] > df['Zero']), df['DownMove'], 0)
+
+    df['plusDI'] = 100 * (df['PlusDM']/df['ATR']).ewm(span=period,min_periods=0,adjust=True,ignore_na=False).mean()
+    df['minusDI'] = 100 * (df['MinusDM']/df['ATR']).ewm(span=period,min_periods=0,adjust=True,ignore_na=False).mean()
+
+    df['ADX'] = 100 * (abs((df['plusDI'] - df['minusDI'])/(df['plusDI'] + df['minusDI']))).ewm(span=period,min_periods=0,adjust=True,ignore_na=False).mean()
